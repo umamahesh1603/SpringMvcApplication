88d8007cb0f23a14fbce6afbfbc61953d0e9fd8e
umamahesh1603 <umamaheshbondalapati8057@gmail.com>
2019-01-08 17:40:45 +0530

    SpringMvcApplication

---
+++ src/main/java/com.stackroute.configuration/HomeInitializer.java
@@ -0,0 +1,27 @@
+package com.stackroute.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class HomeInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses()
+    {
+        return new Class[]{HomeConfiguration.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+
+        return new String[]{"/"};
+
+
+    }
+
+}
